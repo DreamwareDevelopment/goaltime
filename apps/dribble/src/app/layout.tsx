a022ca447d44a0399e7ac50e4771e2ce6ce5fc8f
Dreamware Development <zander@dreamwaredevelopment.com>
2024-10-27 19:14:45 -0700

    Add next ui

--- apps/dribble/src/app/layout.tsx
+++ apps/dribble/src/app/layout.tsx
@@ -11,8 +11,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html suppressHydrationWarning lang="en">
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
