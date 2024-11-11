ff23af0cd59cd606f30a33ebbfffdc3dfda5e16a
Dreamware Development <zander@dreamwaredevelopment.com>
2024-11-11 16:28:40 -0700

    ShadCN integrated

--- apps/goaltime/src/app/page.tsx
+++ apps/goaltime/src/app/page.tsx
@@ -1,9 +1,19 @@
-import Navbar from "../components/navigation";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@goaltime/ui-components";
 
 export default function Index() {
   return (
     <div className="w-full flex flex-col">
-      <Navbar user={undefined} />
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
