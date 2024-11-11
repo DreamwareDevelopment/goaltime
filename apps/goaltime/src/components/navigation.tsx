6b6a5f8a4202991bc817d0dffa09d6f244deb4e2
Dreamware Development <zander@dreamwaredevelopment.com>
2024-11-11 15:04:05 -0700

    Remove nextui

--- apps/goaltime/src/components/navigation.tsx
+++ apps/goaltime/src/components/navigation.tsx
@@ -1,70 +1,18 @@
 import { User } from "@goaltime/shared/models";
-import {
-  Navbar as NextUINavbar, 
-  NavbarBrand, 
-  NavbarContent, 
-  NavbarItem, 
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-  Link,
-  Avatar,
-  Button
-} from "@nextui-org/react";
-
-import ThemeSwitch from "./theme-switcher";
-import { BasketballIcon } from "./icons";
 
 export type NavibarProps = {
   user?: User;
 }
 
 export default function Navbar({ user }: NavibarProps) {
-  const menuItems = {
-    Home: "/",
-    Settings: "/settings",
-    Logout: "/logout"
-  };
-  const avatarName = user?.avatarUrl || user?.username;
+  // const menuItems = {
+  //   Home: "/",
+  //   Settings: "/settings",
+  //   Logout: "/logout"
+  // };
+  // const avatarName = user?.avatarUrl || user?.username;
   return (
-    <NextUINavbar className="py-2 px-0" isBordered shouldHideOnScroll>
-      <NavbarBrand className="hidden md:flex gap-2">
-        <BasketballIcon size={44} />
-        <p className="font-bold text-warning text-inherit">goaltime</p>
-      </NavbarBrand>
-      <NavbarMenuToggle className="md:hidden" />
-      <NavbarContent className="hidden md:flex" justify="center">
-        <Link color="foreground" href="/">
-          Home
-        </Link>
-      </NavbarContent>
-      <NavbarBrand className="md:hidden justify-center">
-        <BasketballIcon size={44} />
-      </NavbarBrand>
-      <NavbarContent justify="end">
-        <NavbarItem>
-          <ThemeSwitch className="pt-1" />
-        </NavbarItem>
-        <NavbarItem>
-          {avatarName ? <Avatar name={avatarName} /> : <Button as={Link} color="warning" href="/login">Login</Button>}
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarMenu>
-        {Object.entries(menuItems).map(([key, value], index) => (
-          <NavbarMenuItem key={`${key}-${index}`}>
-            <Link
-              color={
-                index === 2 ? "primary" : index === Object.keys(menuItems).length - 1 ? "danger" : "foreground"
-              }
-              className="w-full"
-              href={value}
-              size="lg"
-            >
-              {key}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </NextUINavbar>
+    <div className="w-full flex justify-end p-4">
+    </div>
   );
 }
--- apps/goaltime/tailwind.config.js
+++ apps/goaltime/tailwind.config.js
@@ -1,4 +1,3 @@
-const { nextui } = require("@nextui-org/react");
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
 
@@ -10,11 +9,10 @@ module.exports = {
       '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
     ),
     ...createGlobPatternsForDependencies(__dirname),
-    '../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 };
