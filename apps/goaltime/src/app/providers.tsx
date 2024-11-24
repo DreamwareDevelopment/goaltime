c6300e8dc9390c4b1b37b90a980ef4a4d366f63f
Dreamware Development <zander@dreamwaredevelopment.com>
2024-11-24 11:28:54 -0700

    Prevent multiple jotai store instance warning

--- apps/goaltime/src/app/providers.tsx
+++ apps/goaltime/src/app/providers.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import { Provider as JotaiProvider } from "jotai";
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -9,6 +10,8 @@ export interface ProvidersProps {
 
 export function Providers({ children, themeProps }: ProvidersProps) {
   return (
-    <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+    <NextThemesProvider {...themeProps}>
+      <JotaiProvider>{children}</JotaiProvider>
+    </NextThemesProvider>
   );
 }
