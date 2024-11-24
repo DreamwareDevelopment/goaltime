b5d3125ab674896b3ce638bde3a92b0082429f21
Dreamware Development <zander@dreamwaredevelopment.com>
2024-11-24 11:24:41 -0700

    Better layout for analytics card

--- apps/goaltime/src/app/page.tsx
+++ apps/goaltime/src/app/page.tsx
@@ -103,9 +103,11 @@ export default function Dashboard() {
         <ScheduleCard schedule={schedule} />
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
-        <ActionsCard />
-        <GoalyticsCard goals={goals} className="md:col-span-2" />
+      <div className="grid grid-cols-1 gap-6 mt-6">
+        <GoalyticsCard goals={goals} />
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6">
+        <ActionsCard className="col-span-1" />
       </div>
     </div>
   )
