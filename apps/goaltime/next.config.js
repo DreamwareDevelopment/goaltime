994eff4b4c2b491d4185b57fd4d8a4d8328e4461
Dreamware Development <zander@dreamwaredevelopment.com>
2024-12-04 16:54:23 -0700

    Strange nextjs prisma workaround for monorepos

--- apps/goaltime/next.config.js
+++ apps/goaltime/next.config.js
@@ -1,7 +1,19 @@
 //@ts-check
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+// @ts-expect-error PrismaPlugin type is not defined
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
+module.exports = {
+  // @ts-expect-error just ignore the config type
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
+}
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
