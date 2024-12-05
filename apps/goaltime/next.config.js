ecb3606df95df576f08eedfb695bae3b6f99e8db
Dreamware Development <zander@dreamwaredevelopment.com>
2024-12-04 17:08:13 -0700

    Trying to get the prisma hack working

--- apps/goaltime/next.config.js
+++ apps/goaltime/next.config.js
@@ -4,17 +4,6 @@ const { composePlugins, withNx } = require('@nx/next');
 // @ts-expect-error PrismaPlugin type is not defined
 const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
-module.exports = {
-  // @ts-expect-error just ignore the config type
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
-
-    return config
-  },
-}
-
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
@@ -29,6 +18,7 @@ const nextConfig = {
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
+  new PrismaPlugin()
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
