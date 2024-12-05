f3f50fbbc2352eed603bc4895a1e40fdc7f1e1b0
Dreamware Development <zander@dreamwaredevelopment.com>
2024-12-04 18:34:18 -0700

    Still trying

--- apps/goaltime/webpack.config.js
+++ apps/goaltime/webpack.config.js
@@ -1,10 +1,5 @@
-// @ts-expect-error PrismaPlugin type is not defined
 const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
-// eslint-disable-next-line import/no-anonymous-default-export
-export default function (config, options) {
-  const { isServer } = options;
-  if (isServer) {
-    config.plugins = [...config.plugins, new PrismaPlugin()]
-  }
-  return config;
+
+module.exports = {
+  plugins: [new PrismaPlugin()],
 }
