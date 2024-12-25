2e11e6ac7f7d6a1e5e58c13b63464e911c5462fb
Dreamware Development <zander@dreamwaredevelopment.com>
2024-12-25 11:26:34 -0500

    Revert "Temporarily remove cron for production push"
    This reverts commit ae84df34cdf619f29a8a4bd87a323e533fc43fe1.

--- apps/goaltime/src/app/api/inngest/route.ts
+++ apps/goaltime/src/app/api/inngest/route.ts
@@ -1,7 +1,7 @@
 import { serve } from "inngest/next";
-import { checkIn, inngest, postEvent, preEvent, syncGoogleCalendar } from "@/server-utils/inngest";
+import { checkIn, inngest, postEvent, preEvent, syncCalendars, syncGoogleCalendar } from "@/server-utils/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [syncGoogleCalendar, checkIn, preEvent, postEvent],
+  functions: [syncGoogleCalendar, syncCalendars, checkIn, preEvent, postEvent],
 });
