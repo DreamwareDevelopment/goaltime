8ca52f98e34a1091ea678855b5d764b9f41800fc
Dreamware Development <zander@dreamwaredevelopment.com>
2025-01-16 21:15:42 -0700

    Remove unused field

--- libs/shared/src/prisma/migrations/20250115002056_add_zep_fields/migration.sql
+++ libs/shared/src/prisma/migrations/20250115002056_add_zep_fields/migration.sql
@@ -1,3 +1,2 @@
 -- AlterTable
-ALTER TABLE "public"."user_profiles" ADD COLUMN     "last_chat_session_id" UUID,
-ADD COLUMN     "user_facts_session_id" UUID;
+ALTER TABLE "public"."user_profiles" ADD COLUMN     "last_chat_session_id" UUID;
