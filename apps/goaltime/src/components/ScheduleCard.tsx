21d69bda55afbe189a02ce22deeb435bf16a8624
Dreamware Development <zander@dreamwaredevelopment.com>
2025-02-01 23:45:33 -0700

    Temporarily force date for video

--- apps/goaltime/src/components/ScheduleCard.tsx
+++ apps/goaltime/src/components/ScheduleCard.tsx
@@ -60,7 +60,7 @@ export const ScheduleCard = ({ className }: React.HTMLAttributes<HTMLDivElement>
   const profile = useSnapshot(userStore.profile!);
   const [timezone, setTimezone] = useState<string>(profile.timezone);
 
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(dayjs('2025-02-02 17:00').toDate());
   const day = dayjs(date);
   const dayOffset = offsetDay(day, timezone);
   const dateString = dayOffset.format(DATE_FORMAT);
