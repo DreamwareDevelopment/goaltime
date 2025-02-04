da58d894b7def94806ebdf0a9539554118aa6afe
Dreamware Development <zander@dreamwaredevelopment.com>
2025-02-03 19:22:35 -0700

    Forgot to link TOS

--- apps/goaltime/src/components/Landing/Footer.tsx
+++ apps/goaltime/src/components/Landing/Footer.tsx
@@ -5,10 +5,10 @@ const Footer: React.FC = () => {
     <footer className="px-4 lg:px-6 h-14 flex items-center bg-black/50 backdrop-blur-lg w-full z-50">
       <p className="text-xs text-gray-400"><span>© {new Date().getFullYear()} GoalTime</span> <span>All rights reserved.</span></p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-        <a className="text-xs hover:underline underline-offset-4" href="#">
+        <a className="text-xs hover:underline underline-offset-4" href="/terms">
           Terms of Service
         </a>
-        <a className="text-xs hover:underline underline-offset-4" href="#">
+        <a className="text-xs hover:underline underline-offset-4" href="/privacy">
           Privacy Policy
         </a>
       </nav>
